9c06da02555c1907585744356135a5db92247003
3ch0v0 <lijiaqi.echo@gmail.com>
2026-01-15 14:33:40 +0000

    render opaque obj to a FBO

--- 05_DFAOIT/05_DFAOIT/05_DFAOIT.cpp
+++ 05_DFAOIT/05_DFAOIT/05_DFAOIT.cpp
@@ -57,26 +57,16 @@ void setupQuadVertexBuffer(unsigned int& quadVAO, unsigned int& quadVBO)
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
-void setupDFAOITFramebuffer(unsigned int& dfaoitFBO, unsigned int& colorTex, unsigned int& countTex, unsigned int& depthTex)
+void setupDFAOITFramebuffer(unsigned int& opaqueFBO, unsigned int& colorTex,  unsigned int& depthTex)
 {
-	glGenFramebuffers(1, &dfaoitFBO);
+	glGenFramebuffers(1, &opaqueFBO);
 	glGenTextures(1, &colorTex);
-	glGenTextures(1, &countTex);
-	//fragCounterTex:RGB32ui; accTex:RGBA16f; averageTex:RGBA16f; bucketTex:RGBA16f
-	// 
+	
 	//RGB:sum of RGBA
 	glBindTexture(GL_TEXTURE_2D, colorTex);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, scene.windowWidth, scene.windowHeight, 0, GL_RGBA, GL_HALF_FLOAT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-	//R:aplha^2;G:Frragment count
-	glBindTexture(GL_TEXTURE_2D, countTex);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RG32F, scene.windowWidth, scene.windowHeight, 0, GL_RG, GL_HALF_FLOAT, NULL);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
@@ -86,25 +76,26 @@ void setupDFAOITFramebuffer(unsigned int& dfaoitFBO, unsigned int& colorTex, uns
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-	glBindFramebuffer(GL_FRAMEBUFFER, dfaoitFBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, opaqueFBO);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTex, 0);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, countTex, 0);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTex, 0);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, depthTex, 0);
 
 	GLenum drawBuffers[] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
 	glDrawBuffers(2, drawBuffers);
 
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-		std::cout << "ERROR: WBOIT Framebuffer is not complete!" << std::endl;
+		std::cout << "ERROR: DFAOIT Framebuffer is not complete!" << std::endl;
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
-void setupDFAOITTextures(unsigned int& fragmentCounterTex, unsigned int& accTex, unsigned int& averageTex, unsigned int& bucketTex,int w, int h)
+void setupDFAOITTextures(unsigned int& fragmentCounterTex, unsigned int& accTex, unsigned int& averageTex, unsigned int& bucketTex)
 {
 
 
 	glGenTextures(1, &fragmentCounterTex);
 	glBindTexture(GL_TEXTURE_2D, fragmentCounterTex);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32UI, w, h, 0, GL_RGBA_INTEGER, GL_UNSIGNED_INT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32UI, scene.windowWidth, scene.windowHeight, 0, GL_RGBA_INTEGER, GL_UNSIGNED_INT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -112,7 +103,7 @@ void setupDFAOITTextures(unsigned int& fragmentCounterTex, unsigned int& accTex,
 
 	glGenTextures(1, &accTex);
 	glBindTexture(GL_TEXTURE_2D, accTex);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, w, h, 0, GL_RGBA, GL_FLOAT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, scene.windowWidth, scene.windowHeight, 0, GL_RGBA, GL_FLOAT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -120,7 +111,7 @@ void setupDFAOITTextures(unsigned int& fragmentCounterTex, unsigned int& accTex,
 
 	glGenTextures(1, &averageTex);
 	glBindTexture(GL_TEXTURE_2D, averageTex);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, w, h, 0, GL_RGBA, GL_FLOAT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, scene.windowWidth, scene.windowHeight, 0, GL_RGBA, GL_FLOAT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -128,7 +119,7 @@ void setupDFAOITTextures(unsigned int& fragmentCounterTex, unsigned int& accTex,
 
 	glGenTextures(1, &bucketTex);
 	glBindTexture(GL_TEXTURE_2D_ARRAY, bucketTex);	
-	glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGBA16F, w, h, 2, 0, GL_RGBA, GL_FLOAT, NULL);
+	glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGBA16F, scene.windowWidth, scene.windowHeight, 2, 0, GL_RGBA, GL_FLOAT, NULL);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -225,35 +216,36 @@ int main(int argc, char** argv)
 	// Compile shaders
 	unsigned int opaqueShader = CompileShader("opaque.vert", "opaque.frag");
 	unsigned int dfaoitShader = CompileShader("dfaoitAccum.vert", "dfaoitAccum.frag");
-	unsigned int composeShader = CompileShader("compose.vert", "compose.frag");
+	unsigned int composeShader = CompileShader("compose.vert", "dfaoitInfere.frag");
 	//unsigned int debugShader = CompileShader("debug.vert", "debug.frag");
 
 	// quad for compose
 	unsigned int quadVAO, quadVBO;
 	setupQuadVertexBuffer(quadVAO, quadVBO);
 
-	// WBOIT FBO：1. one colorT for accumulate color; 2.another for accummulate alpha 3. one depthT 
-	//unsigned int dfaoitFBO;
-	//unsigned int colorTex, countTex, depthTex;
-	//setupDFAOITFramebuffer(dfaoitFBO, colorTex, countTex, depthTex);
-
+	// DFAOIT ：1.one FBO for opaque 
+	unsigned int opaqueFBO;
+	unsigned int opaqueColorTex, opaqueDepthTex;
+	setupDFAOITFramebuffer(opaqueFBO, opaqueColorTex, opaqueDepthTex);
+	//2. four textures for transparent
 	GLuint fragmentCounterTex;
 	GLuint accTex;
 	GLuint averageTex;
 	GLuint bucketTex;
-	setupDFAOITTextures(fragmentCounterTex, accTex, averageTex, bucketTex,scene.windowWidth, scene.windowHeight);
+	setupDFAOITTextures(fragmentCounterTex, accTex, averageTex, bucketTex);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	//Model TransObj = loadModel("../../resource/chess-set/source/ChessSetTransparent.obj", 0.5f);
-	//Model OpaqueObj = loadModel("../../resource/chess-set/source/ChessSetOpaque.obj", 1.f);
+	
+	Model OpaqueObj = loadModel("../../resource/chess-set/source/ChessSetOpaque.obj", 1.f);
 	//Model BoardObj = loadModel("../../resource/chess-set/source/BoardOpaque.obj", 1.f);
-
+	// 
+	Model TransObj = loadModel("../../resource/chess-set/source/ChessSetTransparent.obj", 0.5f);
 	//Model DragonObj = loadModel("../../resource/dragon.obj", 0.5f);
-	Model MachineObj = loadModel("../../resource/machine2.obj", 0.2f);
+	//Model MachineObj = loadModel("../../resource/machine2.obj", 0.2f);
 
 	//GLuint BoardTex = setup_texture("../../resource/chess-set/textures/ChessboardComplete.bmp");
-	//GLuint ChessTex = setup_texture("../../resource/chess-set/textures/ChessPiece.png");
+	GLuint ChessTex = setup_texture("../../resource/chess-set/textures/ChessPiece.png");
 	GLuint whiteTex = setup_white_texture();
 
 	//InitTimer();
@@ -279,7 +271,11 @@ int main(int argc, char** argv)
 		glm::mat4 projecedLightSpaceMatrix = lightProjection * lightView;
 
 
-		// ========== Pass 1: Render opaque obj to framebuffer0 ==========
+		// ========== Pass 1: Render opaque obj to opaqueFBO ==========
+		glBindFramebuffer(GL_FRAMEBUFFER, opaqueFBO);
+		glViewport(0, 0, scene.windowWidth, scene.windowHeight);
+		glClearColor(bgd[0], bgd[1], bgd[2], bgd[3]);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_DEPTH_TEST);
 		glDepthFunc(GL_LESS);
 		glDepthMask(GL_TRUE);
@@ -287,13 +283,17 @@ int main(int argc, char** argv)
 
 
 		glUseProgram(opaqueShader);
-		//renderOpaqueScene(&OpaqueObj.VAO, opaqueShader, view, projection, OpaqueObj, projecedLightSpaceMatrix, ChessTex, whiteTex);
+		renderOpaqueScene(&OpaqueObj.VAO, opaqueShader, view, projection, OpaqueObj, projecedLightSpaceMatrix, ChessTex, whiteTex);
 		//renderOpaqueScene(&BoardObj.VAO, opaqueShader, view, projection, BoardObj, projecedLightSpaceMatrix, BoardTex, whiteTex);
 
-
+		//debug--begin
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, opaqueFBO);
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0); // 0 是默认屏幕
+		glBlitFramebuffer(0, 0, scene.windowWidth, scene.windowHeight,0, 0, scene.windowWidth, scene.windowHeight,GL_COLOR_BUFFER_BIT, GL_NEAREST);
+		//debug--end
 
 		// ========== Pass 2: Render transparent obj to DFAOIT buffer ==========
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glBindFramebuffer(GL_FRAMEBUFFER, opaqueFBO);
 		//depth test: less; depth writting: false
 		glEnable(GL_DEPTH_TEST);
 		//glDepthFunc(GL_LESS);
@@ -302,16 +302,6 @@ int main(int argc, char** argv)
 
 		//blend mode
 		glDisable(GL_BLEND);
-
-		//initial: colorTex to 0;countTex to 1
-		//glBindFramebuffer(GL_FRAMEBUFFER, dfaoitFBO);
-		//float clearColor0[4] = { 0.f, 0.f, 0.f, 0.f };
-		//glClearBufferfv(GL_COLOR, 0, clearColor0);
-		//glClearBufferfv(GL_COLOR, 1, clearColor0);
-		//glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
-		//glBindFramebuffer(GL_DRAW_FRAMEBUFFER, dfaoitFBO);
-		//glBlitFramebuffer(0, 0, scene.windowWidth, scene.windowHeight, 0, 0, scene.windowWidth, scene.windowHeight, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
-		//glBindFramebuffer(GL_FRAMEBUFFER, dfaoitFBO);
 
 		glUseProgram(dfaoitShader);
 		
@@ -322,14 +312,17 @@ int main(int argc, char** argv)
 
 		unsigned int clearCounter[4] = { 0,0x7F7FFFFF, 0x7F7FFFFF, 0 };
 		glClearTexImage(fragmentCounterTex, 0, GL_RGBA_INTEGER, GL_UNSIGNED_INT, clearCounter);
-		float clearColor[4] = { 0.f,0.f,0.f,0.f };
-		glClearTexImage(accTex, 0, GL_RGBA, GL_FLOAT, clearColor);
-		glClearTexImage(averageTex, 0, GL_RGBA, GL_FLOAT, clearColor);
+		float clearColor01[4] = { 0.f,0.f,0.f,1.f };
+		glClearTexImage(accTex, 0, GL_RGBA, GL_FLOAT, clearColor01);
+		float clearColor00[4] = { 0.f,0.f,0.f,0.f };
+		glClearTexImage(averageTex, 0, GL_RGBA, GL_FLOAT, clearColor00);
+		glClearTexImage(averageTex, 0, GL_RGBA, GL_FLOAT, clearColor00);
+		glClearTexImage(bucketTex, 0, GL_RGBA, GL_FLOAT, clearColor00);
 
 		//---- render loaded transparent model
-		//renderTransparentScene(&TransObj.VAO, dfaoitShader, view, projection, TransObj, projecedLightSpaceMatrix, ChessTex, whiteTex);
+		renderTransparentScene(&TransObj.VAO, dfaoitShader, view, projection, TransObj, projecedLightSpaceMatrix, ChessTex, whiteTex);
 		//renderTransparentScene(&DragonObj.VAO, dfaoitShader, view, projection, DragonObj, projecedLightSpaceMatrix,0,whiteTex);
-		renderTransparentScene(&MachineObj.VAO, dfaoitShader, view, projection, MachineObj, projecedLightSpaceMatrix, 0, whiteTex);
+		//renderTransparentScene(&MachineObj.VAO, dfaoitShader, view, projection, MachineObj, projecedLightSpaceMatrix, 0, whiteTex);
 		
 		//----render loaded transparent model
 		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
@@ -339,23 +332,23 @@ int main(int argc, char** argv)
 		// ========== Pass 3: compose==========
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 		glDisable(GL_DEPTH_TEST);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA);
-		//glDisable(GL_CULL_FACE);
+		glDisable(GL_BLEND);
 
 		glUseProgram(composeShader);
-		glActiveTexture(GL_TEXTURE0);
-		//glBindTexture(GL_TEXTURE_2D, colorTex);
-		//glUniform1i(glGetUniformLocation(composeShader, "colorTex"), 0);
-		//glActiveTexture(GL_TEXTURE1);
-		//glBindTexture(GL_TEXTURE_2D, countTex);
-		//glUniform1i(glGetUniformLocation(composeShader, "countTex"), 1);
+		glBindImageTexture(0, fragmentCounterTex, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32UI);
+		glBindImageTexture(1, accTex, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA16F);
+		glBindImageTexture(2, averageTex, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA16F);
+		glBindImageTexture(3, bucketTex, 0, GL_TRUE, 0, GL_READ_ONLY, GL_RGBA16F);
 
-		glBindVertexArray(quadVAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glActiveTexture(GL_TEXTURE4);
+		glBindTexture(GL_TEXTURE_2D, opaqueColorTex);
+		glUniform1i(glGetUniformLocation(composeShader, "opaqueTex"), 4);
 
-		glDisable(GL_BLEND);
-		glEnable(GL_DEPTH_TEST);
+		//glBindVertexArray(quadVAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		//glDisable(GL_BLEND);
+		//glEnable(GL_DEPTH_TEST);
 
 
 		timer.StopAndDisplay(window, "DFAOIT");
@@ -367,10 +360,13 @@ int main(int argc, char** argv)
 
 	glDeleteVertexArrays(1, &quadVAO);
 	glDeleteBuffers(1, &quadVBO);
-	//glDeleteFramebuffers(1, &dfaoitFBO);
-	//glDeleteTextures(1, &colorTex);
-	//glDeleteTextures(1, &countTex);
-	//glDeleteTextures(1, &depthTex);
+	glDeleteFramebuffers(1, &opaqueFBO);
+	glDeleteTextures(1, &opaqueColorTex);
+	glDeleteTextures(1, &opaqueDepthTex);
+	glDeleteTextures(1, &fragmentCounterTex);
+	glDeleteTextures(1, &accTex);
+	glDeleteTextures(1, &averageTex);
+	glDeleteTextures(1, &bucketTex);
 
 
 	glfwTerminate();
